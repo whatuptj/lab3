6094528ed4b3eeca70d77c3c0a4b17d41dcd0152
whatuptj <130099500+whatuptj@users.noreply.github.com>
2023-04-20 13:35:47 -0700

    XD

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,6 +7,10 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = {1, 2, 3, 4};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{4, 3, 2, 1}, input2);
 	}
 
 
@@ -14,5 +18,8 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+
+    int[] input2 = {1, 2};
+    assertArrayEquals(new int[]{2, 1}, ArrayExamples.reversed(input2));
   }
 }
